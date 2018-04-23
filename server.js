75d5817d8a6b8f036fc251289d84097af2143f3d
dtcrowley <crowleyd1988@gmail.com>
2018-04-23 10:07:42 -0400

    added to server.js

--- server.js
+++ server.js
@@ -20,17 +20,63 @@ app.set("view engine", "handlebars");
 
 mongoose.connect("mongodb://localhost/nyt-scraper")
 
-app.use(require("./routes/apiRoutes"));
-// app.use(require("./routes/htmlRoutes"));
-// app.use(require("./routes/scrapeRoutes"));
-
 app.get("/nyt-scraper", function(req, res) {
     var url = "www.nytimes.com";
     request(url, function(err, res, html){
         var $ = cheerio.load(html);
-       
-    })
-})
+    
+        $("article h2").each(function(i, element){
+            var result={};
+
+            result.title = $(this).children("a").text();
+            result.summary = $(this).children("li").text();
+            result.link = $(this).children("a").attr("href");
+
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    return res.json(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res){
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err){
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function(req, res){
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err){
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function(req, res){
+    db.Comment.create(req.body)
+        .then(function(dbComment){
+            return db.Article.findOneAndUpdate({ _id: req.params.id}, {comment: dbComment._id}, {new: true} );
+        })
+        .then(function(dbArticle){
+            res.json(dbArticle);
+        })
+        .catch(function(err){
+            res.json(err);
+        });
+});  
 
 
 app.listen(PORT, function() {
